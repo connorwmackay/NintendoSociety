797e7f3d4cd420feaab37420cc45845c27cfee0b
Connor Mackay <44244596+connorwmackay@users.noreply.github.com>
2020-11-25 19:33:09 +0000

    Made navbar responsive

---
+++ scripts/navbar.js
@@ -0,0 +1,24 @@
+let navLinks = document.getElementById("navLinks");
+let nav = document.getElementsByTagName("nav")[0];
+let numClicked = 0;
+
+function navMenu() {
+    if (navLinks.style.display === "none") {
+        navLinks.style.display = "block";
+    } else {
+        navLinks.style.display = "none";
+    }
+
+    if (numClicked === 0) {
+        navLinks.style.display = "block";
+        numClicked++;
+    }
+}
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 1299) {
+        navLinks.style.display = "block";
+    } else {
+        navLinks.style.display = "none";
+    }
+});
